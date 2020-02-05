46b1a8166fecda18740fc1b065208afc300fda71
JahazielGuzman <jahaziel.guzman93@gmail.com>
2020-02-04 19:38:30 -0500

    restarting myarticles project

--- myarticles/app/controllers/api/articles_controller.rb
+++
@@ -1,7 +0,0 @@
-
-        class Api::ArticlesController < ApplicationController
-            def index
-                articles = Article.order('created_at DESC')
-                render json: {status: 'SUCCESS', message: 'loaded articles', data: articles}, status: :ok
-            end
-        end
