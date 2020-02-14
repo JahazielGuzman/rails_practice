a3d1c6ee3d9afa0df9c34a50c71791638d6253ff
JahazielGuzman <jahaziel.guzman93@gmail.com>
2020-02-14 15:46:57 -0500

    added rails associations

--- rails-unicorn/app/models/unicorn.rb
+++ rails-unicorn/app/models/unicorn.rb
@@ -1,3 +1,15 @@
 class Unicorn < ApplicationRecord
-    validates :name, presence: true
+    has_many :quests
+    has_many :leprechauns, through: :quests
+
+    validates :name, presence: true, uniqueness: true, length: {maximum: 10}
+    validates :shoe, presence: true
+    validates :horn, presence: true
+    validate :name_cannot_begin_with_x 
+
+    def name_cannot_begin_with_x
+        if self.name[0].downcase == "x"
+            self.errors.add(:name, "cannot begin with X")
+        end
+    end
 end
