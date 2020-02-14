8a8ac4d8c151372d35cdfdac90a5200e100ae990
JahazielGuzman <jahaziel.guzman93@gmail.com>
2020-02-14 15:50:14 -0500

    added routes for leprechaun and quests

--- rails-unicorn/config/routes.rb
+++ rails-unicorn/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :quests, only: [:index, :create, :show, :update, :destroy]
   # resources :unicorns
-  resources :unicorns, only: [:index, :create, :update, :destroy]
+  resources :unicorns, only: [:index, :create, :show, :update, :destroy]
+
 end
